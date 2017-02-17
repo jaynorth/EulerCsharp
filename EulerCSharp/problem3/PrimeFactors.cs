ee0ea2d7d1a307e50ff991c6bb18c2c938c3bec5
jay <jayrdee@gmail.com>
2017-02-17 19:30:43 +0100

    problem 3 too slow, not finished

---
+++ EulerCSharp/problem3/PrimeFactors.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem3
+{
+    class PrimeFactors
+    {
+
+
+        public static List<int> PrimeFactorNumbers(long number) {
+
+            List<int> ListeFactors = new List<int>();
+
+            bool prime ;
+            int lastPrime=0;
+
+         
+
+            //Console.WriteLine("new number is " + number);
+
+            for (int i = 3; i*i < number; i = i + 2) {
+                prime = false;
+
+                if (number % i == 0) {
+
+                    for (int j = 2; j < i; j++) {
+
+                        if (i % j == 0)
+                        {
+                            prime = false;
+                            
+                            break;
+                        }
+                        else {
+                            prime = true;
+                            lastPrime = i;
+                            
+                        }
+                    }
+                    
+                }
+
+                if (prime == true) {
+                    ListeFactors.Add(i);
+                }
+            }
+
+            return ListeFactors;
+        }
+
+        public static void ReadList(List<int> ListeFactors) {
+            foreach (int number in ListeFactors) {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ EulerCSharp/problem3/Program.cs
@@ -0,0 +1,44 @@
+﻿using Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DisplayScreen pb3display = new DisplayScreen();
+            pb3display.ProblemTitle = "Problem 3";
+            pb3display.ProblemHeader = "Largest prime factor";
+            pb3display.Description = "The prime factors of 13195 are 5, 7, 13 and 29.\n What is the largest prime factor of the number 600851475143 ?";
+            pb3display.DisplayHeader();
+
+            DateTime t1 = DateTime.Now;
+            
+
+            long number = 600851475143; //600851475143 13195
+
+            List<int> List = PrimeFactors.PrimeFactorNumbers(number);
+            //PrimeFactors.ReadList(List);
+            try
+            {
+                Console.WriteLine("Largest Prime Factor for " + number + " is : " + List.Max());
+                DateTime t2 = DateTime.Now;
+                Console.WriteLine("Timer : " + (t2 - t1));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("There was an error: " + ex.Message);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
