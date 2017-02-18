fd5658a22301d35fa08299e6b4fd0af6b2a606c5
jay <jayrdee@gmail.com>
2017-02-18 01:05:39 +0100

    problem7 done

---
+++ EulerCSharp/problem7/PrimeNumberOrder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem7
+{
+    class PrimeNumberOrder
+    {
+
+        public static int PrimeNumberST(int orderNumber) {
+            int prime =5;
+            int count = 3;
+            int i = 5;
+            bool isprime = false;
+
+            while (count <= orderNumber) {
+
+                for(int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isprime = false;
+                        i = i + 2;
+                        j = 2;
+
+                    }
+                    else {
+                        isprime = true;
+                    }
+                    
+                }
+                
+                if (isprime) {
+                    count++;
+                    //Console.WriteLine("count is : " + count);
+                    i = i + 2;
+                }
+            }
+
+            
+
+            prime = i - 2;
+            return prime;
+        }
+    }
+}
---
+++ EulerCSharp/problem7/Program.cs
@@ -0,0 +1,35 @@
+﻿using Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayScreen pb7display = new DisplayScreen();
+            pb7display.ProblemTitle = "Problem 7";
+            pb7display.ProblemHeader = "10001st prime";
+            pb7display.Description = "By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.\nWhat is the 10 001st prime number ? ";
+            pb7display.DisplayHeader();
+
+            //////////////////////////////////////////////////////////////////
+
+
+
+            int order = 10001;
+            
+            int result = PrimeNumberOrder.PrimeNumberST(order);
+            pb7display.DisplayAnswer("Prime number " + order + " is  :", result);
+
+
+            pb7display.DisplayFooter();
+            Console.ReadKey();
+
+        }
+    }
+}
