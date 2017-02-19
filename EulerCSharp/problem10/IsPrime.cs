0f97ce2777d76733b82dab15f84854fb751002f9
jay <jayrdee@gmail.com>
2017-02-19 02:31:56 +0100

    problem10 done

---
+++ EulerCSharp/problem10/IsPrime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem10
+{
+    class IsPrime
+    {
+        
+
+
+        public static bool CheckIfPrime(int number) {
+          //no need to check divisions above square root, reduces computing time from 7 minutes to less than a second
+            double limit = Math.Sqrt(number);
+          //
+
+          for(int i=3;i<limit+1; i=i+2 )//+2 steps because no need to check even numbers, 2 is the only one
+          {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+                    
+          }
+            return true;
+        }
+
+        public static  long  PrimeListSum(List<long> primeList) {
+            long sum = 0;
+            foreach (int item in primeList)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ EulerCSharp/problem10/Program.cs
@@ -0,0 +1,40 @@
+﻿using Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DisplayScreen pb10display = new DisplayScreen();
+            pb10display.ProblemTitle = "Problem 10";
+            pb10display.ProblemHeader = "Summation of primes";
+            pb10display.Description = "The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.\nFind the sum of all the primes below two million.";
+            pb10display.DisplayHeader();
+
+            //////////////////////////////////////////////////////////////////
+
+            bool check ;
+            int limit = 2000000;
+            long sum = 5;//sum of 2 and 3
+            for (int i = 5; i <= limit; i=i+2) {
+                check = IsPrime.CheckIfPrime(i);
+                if (check) {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine("\nsum of primes up to " + limit + " is : " + sum );
+            
+            /////////////////////////////////////////////////////////////////
+            pb10display.DisplayFooter();
+            Console.ReadKey();
+        }
+    }
+}
