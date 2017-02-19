c55139f5ecfb20408f6e9d86912d8ceaecd15c84
jay <jayrdee@gmail.com>
2017-02-19 18:57:30 +0100

    VS is bugging

---
+++ EulerCSharp/problem11/Grid.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem11
+{
+    class Grid
+    {
+         public static string[,] PopulateArrayFromStream2D(int dimension, StreamReader sr) {
+            string[,] array = new string[dimension,dimension];//for 20x20 grid
+            string line ;
+            //int number;
+            int count;
+
+            for(int i = 0; i< dimension; i++){
+                count = 0;
+                line = sr.ReadLine();
+                
+                string [] tab =line.Split(' ');
+                foreach (string item in tab) {
+                    //int element = int.Parse(item);
+                    array[i, count] = item;
+                    count++;
+                }
+                
+                 //number = array[i, 1];
+                //sr.ReadLine();
+            }
+            return array;
+
+        }
+
+        public static void DisplayGrid(string[,] grid) {
+            int count = 0;
+            foreach(string row in grid) {
+                if (count % 20 == 0) { Console.WriteLine(); }
+                Console.Write(row + " ");
+                count++;
+            }
+        }
+
+        public static long BiggestProductRight(string[,] grid, int adjacentNumbers) {
+            long product = 1;
+            int number;
+            long biggestProduct= 0;
+            int lengthRow = 20;
+            for (int k = 0; k < 20; k++)
+            {
+                for (int j = 0; j < lengthRow - adjacentNumbers; j++)
+                {
+                    for (int i = j; i < adjacentNumbers + j; i++)
+                    {
+                        number = int.Parse(grid[k, i]);
+                        product *= number;
+                    }
+                    if (product > biggestProduct)
+                    {
+                        biggestProduct = product;
+                        
+                    }
+                    product = 1;
+                }
+            }
+            
+            return biggestProduct; 
+        }
+
+        public static long BiggestProductDown(string[,] grid, int adjacentNumbers)
+        {   //table [row][colum]
+            long product = 1;
+            int number;
+            long biggestProduct = 0;
+            int lengthRow = 20;
+            for (int k = 0; k < 20; k++)
+            {
+                for (int j = 0; j < lengthRow - adjacentNumbers; j++)
+                {
+                    for (int i = j; i < adjacentNumbers + j; i++)//Column
+                    {
+                        number = int.Parse(grid[i, k]); //by just switching k and i it does the same as lef/right to up/down
+                        product *= number;
+                    }
+                    if (product > biggestProduct)
+                    {
+                        biggestProduct = product;
+
+                    }
+                    product = 1;
+                }
+            }
+
+            return biggestProduct;
+        }
+    }
+}
---
+++ EulerCSharp/problem11/Program.cs
@@ -0,0 +1,48 @@
+﻿using Display;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DisplayScreen pb11display = new DisplayScreen();
+            pb11display.ProblemTitle = "Problem 11";
+            pb11display.ProblemHeader = "Largest product in a grid";
+            pb11display.Description = "In the 20×20 grid below, four numbers along a diagonal line have been marked in red.\nThe product of these numbers is 26 × 63 × 78 × 14 = 1788696.\nWhat is the greatest product of four adjacent numbers in the same direction(up, down, left, right, or diagonally) in the 20×20 grid ? ";
+            pb11display.DisplayHeader();
+
+            //////////////////////////////////////////////////////////////////
+            // Creating Text File with grid
+            // grid.txt
+            // moved it to bin/debug
+            
+            string filePath = @"grid.txt"; 
+            StreamReader sr = new StreamReader(filePath);
+
+            string[,] grid = Grid.PopulateArrayFromStream2D(20, sr);
+            
+            Grid.DisplayGrid(grid);
+            //Console.WriteLine(grid[2, 2]);
+
+            long productRight = Grid.BiggestProductRight(grid, 4);
+            Console.WriteLine("\nProduct is :" + productRight);
+            //long productDown = Grid.BiggestProductDown(grid, 4);
+            //Console.WriteLine("\nProduct is :" + productDown);
+
+
+            sr.Close();
+            // pb11display.DisplayAnswer("Prime number " + order + " is  :", result);
+
+            pb11display.DisplayFooter();
+            Console.ReadKey();
+        }
+    }
+}
