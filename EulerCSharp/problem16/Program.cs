4a15ed9bd802f912c5120e9fdb36ad54dbbb474c
jay <jayrdee@gmail.com>
2017-02-21 00:29:51 +0100

    problem16 done, added syste.numerics namespace

---
+++ EulerCSharp/problem16/Program.cs
@@ -0,0 +1,41 @@
+﻿using Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace problem16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DisplayScreen pb16display = new DisplayScreen();
+            pb16display.ProblemTitle = "Problem 16";
+            pb16display.ProblemHeader = "Power digit sum";
+            pb16display.Description = "2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.\nWhat is the sum of the digits of the number 21000 ? ";
+            pb16display.DisplayHeader();
+
+
+            //////////////////////////////////////////////////////////////////
+            int sum = 0;
+            int power = 1000;
+            BigInteger number = 2;
+            number = BigInteger.Pow(number, power);
+            string s= number.ToString();
+            //Console.WriteLine(s);
+            foreach (char item in s) {
+                
+                sum += Convert.ToInt32(char.GetNumericValue(item));//converts char to double then converts double to int32
+            }
+
+            Console.WriteLine("sum is : " + sum);
+            //////////////////////////////////////////////////////////////////
+            pb16display.DisplayFooter();
+            Console.ReadKey();
+        }
+    }
+}
